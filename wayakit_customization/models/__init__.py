ada6267726fb6ab1c9b15dc0bca55067d9b5a7f5
Awais <awais.indexworld@gmail.com>
2024-10-01 15:02:30 +0500

    make flow for vehicles in services api

--- wayakit_customization/models/__init__.py
+++ wayakit_customization/models/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import product_inherit
+from . import service_type
+from . import appointment_type
---
+++ wayakit_customization/models/service_type.py
@@ -0,0 +1,45 @@
+from odoo import fields, models, api
+
+class ServiceType(models.Model):
+    _name = "service.type"
+    _description = "Service Type"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    name = fields.Selection([
+        ('vehicle', 'Vehicle'),
+        ('curtain', 'Curtain'),
+    ], string='Service Type')
+    vehicle_type = fields.Selection([
+        ('car', 'Car'),
+        ('bike', 'Bike')
+    ], string='Vehicle Type')
+    in_use = fields.Selection([
+        ('personal', 'Personal'),
+        ('business', 'Business')
+    ], string='In use')
+    sub_type_ids = fields.One2many(
+        'vehicle.type',
+        'service_type_id',
+    )
+
+
+class VehicleType(models.Model):
+    _name = "vehicle.type"
+    _description = "Vehicle Type"
+
+    name = fields.Char()
+    price = fields.Float()
+    inclusive_tax_price = fields.Float(compute='_compute_inclusive_tax_price', store=True)
+    tax = fields.Many2one('account.tax')
+    service_type_id = fields.Many2one('service.type')
+
+    @api.depends('price', 'tax')
+    def _compute_inclusive_tax_price(self):
+        for rec in self:
+            if rec.tax and rec.price:
+                tax_value = (rec.tax.amount/100) * rec.price
+                rec.inclusive_tax_price = rec.price + tax_value
+            else:
+                rec.inclusive_tax_price = rec.price
+
+
