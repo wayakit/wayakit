6a48b82b51f5a8b8e880c09ea5b683658ae937e6
Maria Fernanda Morales <112129254+cyberialyr777@users.noreply.github.com>
2025-09-29 14:09:13 -0600

    added columns and removed CUD permissions

--- price_intelligence/models/product_price_suggestion.py
+++ price_intelligence/models/product_price_suggestion.py
@@ -5,7 +5,10 @@ from odoo import models, fields
 class ProductPriceSuggestion(models.Model):
     _name = 'product.price.suggestion'
     _description = 'Product Price Suggestion'
+    _order = 'last_update_date desc'  # Ordena los registros por fecha, los más nuevos primero
 
+    # --- Campos reordenados ---
+    last_update_date = fields.Datetime(string='Last Update Date', readonly=True, default=fields.Datetime.now)
     product_id_str = fields.Char(string='Product ID', required=True)
     product_type = fields.Char(string='Product Type')
     generic_product_type = fields.Char(string='Generic Product Type')
@@ -15,4 +18,3 @@ class ProductPriceSuggestion(models.Model):
     production_cost = fields.Float(string='Production Cost')
     suggested_price = fields.Float(string='Suggested Sale Price')
     profit = fields.Float(string='Profit')
-    last_update_date = fields.Datetime(string='Last Update Date', readonly=True, default=fields.Datetime.now)
--- wayakit_customization/__manifest__.py
+++ wayakit_customization/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product', 'appointment', 'appointment_account_payment', 'crm'],
+    'depends': ['base', 'product', 'appointment', 'appointment_account_payment'],
 
     # always loaded
     'data': [
