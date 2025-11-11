e8ca008596ffe4bf68c2f1d3154cc6a26d2fb720
Maria Fernanda Morales <112129254+cyberialyr777@users.noreply.github.com>
2025-11-11 11:42:18 -0600

    FIX: wayakit_customization

--- wayakit_customization/models/product_inherit.py
+++ wayakit_customization/models/product_inherit.py
@@ -1,6 +1,8 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     duration = fields.Float(string="Duration", default=0)
+
+    master_product_id = fields.Many2one('product.master', string="Master Product (Temporal)")
