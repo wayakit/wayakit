4b1cbd68c33c2ee7a0eab532ba0037c0ef7a144f
Maria Fernanda Morales <112129254+cyberialyr777@users.noreply.github.com>
2025-11-11 11:46:31 -0600

    FIX: definitive wayakit_customization

--- wayakit_customization/models/product_inherit.py
+++ wayakit_customization/models/product_inherit.py
@@ -4,5 +4,3 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     duration = fields.Float(string="Duration", default=0)
-
-    master_product_id = fields.Many2one('product.master', string="Master Product (Temporal)")
