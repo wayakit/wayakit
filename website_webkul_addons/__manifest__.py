8044ce31801be0fc6ed257f438d0a765d1948057
Abdul Rehman <arehman.indexworld@gmail.com>
2026-04-20 18:16:21 +0500

    added new module

---
+++ website_webkul_addons/__manifest__.py
@@ -0,0 +1,50 @@
+#############################################################################
+# Author      : Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+# Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://store.webkul.com/license.html/>
+##############################################################################
+{
+    'name':  'Website Webkul Addons',
+    'summary':  """Website Webkul Addons allows Odoo users
+                    to manage all Webkul’s Odoo website Related
+                    modules from single page.
+                 """,
+    'category':  'Hidden',
+    'version':  '1.0.2',
+    'author':  'Webkul Software Pvt. Ltd.',
+    'license':  'Other proprietary',
+    'website':  'https://store.webkul.com/Odoo.html',
+    'description':  """Website Webkul Addons
+                              Webkul
+                              Addons
+                              Website Addons
+                              Webkul Addons
+                              Webkul Website Modules
+                              Webkul Module
+                              Modules
+                              Webkul Addons For Website""",
+    'live_test_url':  (
+        'http://odoodemo.webkul.com/'
+        '?module=website_webkul_addons'
+    ),
+    'depends':  ['website'],
+    'data':  [
+        'security/ir.model.access.csv',
+        'wizard/wk_website_wizard_view.xml',
+        'views/webkul_addons_config_view.xml',
+    ],
+    'images':  ['static/description/Banner.png'],
+    'application':  True,
+    'installable':  True,
+    'auto_install':  False,
+    'pre_init_hook': 'pre_init_check',
+}
