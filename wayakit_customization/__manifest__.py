a6ea0be5eb7bc1b121972e6f1a65493233822418
Maria Fernanda Morales <112129254+cyberialyr777@users.noreply.github.com>
2026-05-14 13:56:54 -0600

    forcing update of module

--- wayakit_customization/__manifest__.py
+++ wayakit_customization/__manifest__.py
@@ -17,10 +17,10 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.6',
+    'version': '0.7',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product', 'appointment', 'appointment_account_payment', 'price_intelligence'],
+    'depends': ['base', 'product', 'appointment', 'appointment_account_payment', 'price_intelligence', 'website'],
 
     # always loaded
     'data': [
