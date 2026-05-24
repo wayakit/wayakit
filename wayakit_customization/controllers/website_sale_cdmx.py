08f12d6237488800775a0749a325d4ed1fa652ea
Maria Fernanda Morales <112129254+cyberialyr777@users.noreply.github.com>
2026-05-24 17:59:54 -0600

    [ADD] Restrict shipping states to CDMX only on WAYAKIT MX website

--- wayakit_customization/controllers/website_sale_cdmx.py
+++ wayakit_customization/controllers/website_sale_cdmx.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Override del endpoint /shop/country_infos para filtrar los estados
-devueltos al formulario de dirección, limitándolos a CDMX cuando
-el website activo es 'WAYAKIT MX'.
-"""
-
 from odoo import http
 from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 WAYAKIT_MX_WEBSITE_NAME = 'WAYAKIT MX'
-
-# IDs de todos los registros que representan CDMX en esta instancia
-CDMX_STATE_IDS = {493, 1684, 1391}
+CDMX_VISIBLE_ID = 493  # Ciudad de México, CMX - único visible en el dropdown
 
 
 class WebsiteSaleCDMXFilter(WebsiteSale):
@@ -25,21 +17,16 @@ class WebsiteSaleCDMXFilter(WebsiteSale):
         website=True,
     )
     def country_infos(self, country, mode, **kw):
-        """
-        Intercepts the standard country_infos endpoint.
-        When on WAYAKIT MX, filters returned states to CDMX only.
-        """
         result = super().country_infos(country, mode, **kw)
 
         website = request.website
         if not website or website.name != WAYAKIT_MX_WEBSITE_NAME:
             return result
 
-        # result['states'] es una lista de tuplas (id, name, code)
         if result.get('states'):
             result['states'] = [
                 state for state in result['states']
-                if state[0] in CDMX_STATE_IDS
+                if state[0] == CDMX_VISIBLE_ID
             ]
 
         return result
