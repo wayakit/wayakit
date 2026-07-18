cd8260dc326049cc5606bb438dfb34d051649fb8
Maria Fernanda Morales <112129254+cyberialyr777@users.noreply.github.com>
2026-07-18 16:00:06 -0600

    add: quality module customization for pH

---
+++ quality_custom/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Quality Custom',
+    'version': '17.0.1.0.0',
+    'summary': 'Quality extended: tolerance range on worksheet-type control points',
+    'author': 'Your Company',
+    'category': 'Manufacturing/Quality',
+    'license': 'LGPL-3',
+    'depends': [
+        'quality_control_worksheet',
+    ],
+    'data': [
+        'views/quality_point_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
